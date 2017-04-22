54e58954d037947e49101e71a75cb0ada2dae209
Ruizhe Zhao <vincentzhaorz@gmail.com>
2017-04-22 09:39:01 -0400

    initialized TensorFlow command line tool

---
+++ main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import tensorflow as tf
+
+def main(_):
+    pass
+
+if __name__ == '__main__':
+    tf.app.run()
