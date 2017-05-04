aac4c6d6c2ab00d6d262928f4964d3b9cd94ffb5
Ruizhe Zhao <vincentzhaorz@gmail.com>
2017-05-04 12:39:16 -0400

    some updates to the CIFAR-10 model

--- models/cifar10/cifar10.py
+++ models/cifar10/cifar10.py
@@ -2,7 +2,17 @@ import re
 import tensorflow as tf
 import cifar10_input
 
+IMAGE_SIZE = cifar10_input.IMAGE_SIZE
 NUM_CLASSES = cifar10_input.NUM_CLASSES
+NUM_EXAMPLES_PER_EPOCH_FOR_TRAIN = cifar10_input.NUM_EXAMPLES_PER_EPOCH_FOR_TRAIN
+NUM_EXAMPLES_PER_EPOCH_FOR_EVAL = cifar10_input.NUM_EXAMPLES_PER_EPOCH_FOR_EVAL
+
+# Constants describing the training process.
+MOVING_AVERAGE_DECAY = 0.9999     # The decay to use for the moving average.
+NUM_EPOCHS_PER_DECAY = 350.0      # Epochs after which learning rate decays.
+LEARNING_RATE_DECAY_FACTOR = 0.1  # Learning rate decay factor.
+INITIAL_LEARNING_RATE = 0.1       # Initial learning rate.
+
 FLAGS = tf.app.flags.FLAGS
 
 TOWER_NAME = 'tower'
@@ -100,4 +110,59 @@ def inference(images):
 
     return softmax_linear 
 
+def loss(logits, labels):
+    labels = tf.cast(labels, tf.int64)
+    cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(
+            labels=labels, logits=logits, name='cross_entropy_per_example')
+    cross_entropy_mean = tf.reduce_mean(cross_entropy, name='cross_entropy')
+    tf.add_to_collection('losses', cross_entropy_mean)
 
+    return tf.add_n(tf.get_collection('losses'), name='total_loss')
+
+def _add_loss_summaries(total_loss):
+    loss_averages = tf.train.ExponentialMovingAverage(0.9, name='avg')
+    losses = tf.get_collection('losses')
+    loss_averages_op = loss_averages.apply(losses + [total_loss])
+
+    for l in losses + [total_loss]:
+        tf.summary.scalar(l.op.name + '(raw)', l)
+        tf.summary.scalar(l.op.name, loss_averages.average(l))
+    return loss_averages_op
+
+def train(total_loss, global_step):
+    num_batches_per_epoch = NUM_EXAMPLES_PER_EPOCH_FOR_TRAIN // FLAGS.batch_size
+    decay_steps = int(num_batches_per_epoch * NUM_EPOCHS_PER_DECAY)
+
+    lr = tf.train.exponential_decay(INITIAL_LEARNING_RATE,
+                                    global_step,
+                                    decay_steps,
+                                    LEARNING_RATE_DECAY_FACTOR,
+                                    staircase=True)
+    tf.summary.scalar('learning_rate', lr)
+
+    loss_average_op = _add_loss_summaries(total_loss)
+    
+    with tf.control_dependencies([loss_average_op]):
+        opt = tf.train.GradientDescentOptimizer(lr)
+        grads = opt.compute_gradients(total_loss)
+
+    apply_gradient_op = opt.apply_gradients(grads, global_step=global_step)
+
+    for var in tf.trainable_variables():
+        tf.summary.histogram(var.op.name, var)
+
+    for grad, var in grads:
+        if grad is not None:
+            tf.summary.histogram(var.op.name + '/gradients', grad)
+
+    variable_averages = tf.train.ExponentialMovingAverage(
+            MOVING_AVERAGE_DECAY, global_step)
+    variable_averages_op = variable_averages.apply(tf.trainable_variables())
+
+    with tf.control_dependencies([apply_gradient_op, variable_averages_op]):
+        train_op = tf.no_op(name='train')
+
+    return train_op
+
+def eval(logits, labels):
+    pass
--- models/cifar10/cifar10_run.py
+++ models/cifar10/cifar10_run.py
@@ -5,6 +5,7 @@ from __future__ import division
 from __future__ import print_function
 
 import tensorflow as tf
+import time
 
 FLAGS = tf.app.flags.FLAGS
 tf.app.flags.DEFINE_string('data_dir',
@@ -16,6 +17,9 @@ tf.app.flags.DEFINE_string('log_dir',
 tf.app.flags.DEFINE_integer('batch_size',
                             128,
                             """Size of a batch""")
+tf.app.flags.DEFINE_integer('max_step',
+                            20000,
+                            """Max number of training steps""")
 tf.app.flags.DEFINE_boolean('use_fp16',
                             False,
                             """Half float""")
@@ -23,12 +27,40 @@ tf.app.flags.DEFINE_boolean('use_fp16',
 import cifar10
 
 def main(_):
-    images, labels = cifar10.distorted_inputs()
-    logits = cifar10.inference(images)
-    with tf.Session() as sess:
-        sess.run(tf.global_variables_initializer())
+    with tf.Graph().as_default():
+        global_step = tf.contrib.framework.get_or_create_global_step()
 
-        writer = tf.summary.FileWriter(FLAGS.log_dir, sess.graph)
+        images, labels = cifar10.distorted_inputs()
+
+        logits = cifar10.inference(images)
+
+        loss = cifar10.loss(logits, labels)
+
+        train_op = cifar10.train(loss, global_step)
+
+        with tf.Session() as sess:
+            sess.run(tf.global_variables_initializer())
+            coord = tf.train.Coordinator()
+            threads = tf.train.start_queue_runners(coord=coord)
+
+            summary = tf.summary.merge_all()
+            writer = tf.summary.FileWriter(FLAGS.log_dir, sess.graph)
+
+            print('Training started')
+            for i in range(FLAGS.max_step):
+                start_time = time.time()
+                _, loss_value = sess.run([train_op, loss])
+                duration = time.time() - start_time
+
+                if i % 100 == 0:
+                    print('Step %6d: loss = %.2f (%.3f sec)' %
+                          (i, loss_value, duration))
+                    summary_str = sess.run(summary)
+                    writer.add_summary(summary_str, i)
+                    writer.flush()
+
+            coord.request_stop()
+            coord.join(threads)
 
 if __name__ == '__main__':
     print('TensorFlow version %s' % tf.__version__)
